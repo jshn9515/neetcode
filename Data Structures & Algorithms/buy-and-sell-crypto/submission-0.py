67056f37df2a3a754111c5a30aaa5d9cef0b7890
jshn9515 <jshn9510@gmail.com>
2026-07-28 20:36:20 +0000

    Fix type hint for prices parameter in maxProfit method

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -1,11 +1,11 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
         l, r = 0, 0
         max_profit = 0
         while r < len(prices) - 1:
             r += 1
-            if prices[r] - prices[l] > 0:
-                profit = prices[r] - prices[l]
+            profit = prices[r] - prices[l]
+            if profit > 0:
                 max_profit = max(profit, max_profit)
             else:
                 l = r
