816986f4b252b9555fd37d0bdd21e01f8f03e37a
jshn9515 <jshn9510@gmail.com>
2026-07-28 20:46:34 +0000

    Add a blank line for improved readability in maxProfit method

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -2,6 +2,7 @@ class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         l, r = 0, 0
         max_profit = 0
+
         while r < len(prices) - 1:
             r += 1
             profit = prices[r] - prices[l]
